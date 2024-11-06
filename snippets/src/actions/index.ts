c660c2ceb8d446dd39bc53b1bb8ff8c400e8f135
Mikaela Hedberg <mie@du.se>
2024-11-06 12:55:37 +0100

    error handling with formstate

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -2,6 +2,31 @@
 import { redirect } from 'next/navigation';
 import { db } from '@/db';
 
+export async function createSnippet(actionState: { message: string }, formData: FormData) {
+  //this needs to be a server action
+  //directive used by nextjs to tell it it's a server action.
+  //formData-object of type FormData (typescript)
+  const title = formData.get('title');
+  const code = formData.get('code');
+  if (typeof title !== 'string' || title.length < 3) {
+    return { message: 'Title must be longer.' };
+  }
+  if (typeof code !== 'string' || code.length < 10) {
+    return { message: 'Code must be longer.' };
+  }
+  //create a new record in the database
+  /*   const snippet = await db.snippet.create({
+    data: {
+      title,
+      code
+    }
+  }); 
+  //this is server side, log appears in terminal
+  console.log(snippet);*/
+
+  //redirect
+  redirect('/');
+}
 export async function editSnippet(id: number, code: string) {
   await db.snippet.update({ where: { id }, data: { code } });
   redirect(`/snippets/${id}`);
--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,5 +1,8 @@
-import { redirect } from 'next/navigation';
-import { db } from '@/db';
+'use client';
+import { useActionState } from 'react';
+
+import * as actions from '@/actions';
+
 /* Pages are by default server components (use client for client components)
  * can use async/await
  * can't use hooks
@@ -7,46 +10,49 @@ import { db } from '@/db';
  *
  * client component can't show server components as children. rendered once on the server.
  */
-function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    //this needs to be a server action
-    //directive used by nextjs to tell it it's a server action.
-    'use server';
-    //formData-object of type FormData (typescript)
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-    //create a new record in the database
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code
-      }
-    });
-    //this is server side, log appears in terminal
-    console.log(snippet);
-    //redirect
-    redirect('/');
-  }
 
+/* Wraps action with additional functionality and a state that can be sent to and from server from client component.  */
+
+function SnippetCreatePage() {
+  const [formState, action] = useActionState(actions.createSnippet, { message: '' });
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold text-xl my-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
-          <label htmlFor="title" className="w-12">
+          <label
+            htmlFor="title"
+            className="w-12">
             Title
           </label>
-          <input name="title" id="title" className="border rounded p-2 w-full" type="text" />
+          <input
+            name="title"
+            id="title"
+            className="border rounded p-2 w-full"
+            type="text"
+          />
         </div>
 
         <div className="flex gap-4">
-          <label htmlFor="PrismaClient" className="w-12">
+          <label
+            htmlFor="PrismaClient"
+            className="w-12">
             Code
           </label>
-          <textarea className="border rounded p-2 w-full font-mono" name="code" id="code"></textarea>
+          <textarea
+            className="border rounded p-2 w-full font-mono"
+            name="code"
+            id="code"></textarea>
         </div>
-        <button type="submit" className="rounded p-2 bg-blue-200">
-          Send
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-200 border-2 border-red-400 rounded">{formState.message}</div>
+        ) : (
+          ''
+        )}
+        <button
+          type="submit"
+          className="rounded p-2 bg-blue-200">
+          Create
         </button>
       </div>
     </form>
