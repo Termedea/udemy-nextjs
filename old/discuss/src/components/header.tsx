60696a5ec56ae8c21100bfa3b3ae56d73c9470d5
Mikaela Hedberg <mie@du.se>
2026-05-21 12:00:52 +0200

    latest

---
+++ old/discuss/src/components/header.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar
+} from '@nextui-org/react';
+import { auth } from '@/auth';
+import * as actions from '@/actions';
+
+export default async function Header() {
+  const session = await auth();
+
+  let authContent: React.ReactNode;
+  if (session?.user) {
+    authContent = <Avatar src={session.user.image || ''} />;
+  } else {
+    authContent = (
+      <>
+        <NavbarItem className="flex gap-3">
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign In
+            </Button>
+          </form>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="flat">
+              Sign Up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">{authContent}</NavbarContent>
+    </Navbar>
+  );
+}
